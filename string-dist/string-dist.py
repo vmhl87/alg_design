236ec2b47d2c82f3bfbb78a34df2a0b06a67eb85
Vincent Loh <vincentmloh87@gmail.com>
2024-04-23 13:23:21 -0400

    proper fix

--- string-dist/string-dist.py
+++ string-dist/string-dist.py
@@ -1,24 +1,22 @@
 # string distance, bottom up dynamic
 
 def string_distance(s1, s2):
-    s1, s2 = '_' + s1, '_' + s2
+    distances = [[0 for j in range(len(s1)+1)] for i in range(len(s2)+1)]
 
-    distances = [[0 for j in range(len(s1))] for i in range(len(s2))]
+    for j in range(len(s1)+1):
+        distances[0][j] = j
 
-    for j in range(len(s1)):
-        distances[0][j] = j + (0 if s1[0]==s2[0] else 1)
+    for i in range(1, len(s2)+1):
+        distances[i][0] = i
 
-    for i in range(1, len(s2)):
-        distances[i][0] = i + (0 if s1[0]==s2[0] else 1)
-
-    for i in range(1, len(s2)):
-        for j in range(1, len(s1)):
-            distances[i][j] = min(
+    for i in range(len(s2)):
+        for j in range(len(s1)):
+            distances[i+1][j+1] = min(
                     min(
-                        distances[i-1][j]+1,
-                        distances[i][j-1]+1
+                        distances[i+1][j]+1,
+                        distances[i][j+1]+1
                     ),
-                    distances[i-1][j-1]+(0 if s2[i]==s1[j] else 1)
+                    distances[i][j]+(0 if s2[i]==s1[j] else 1)
                 )
 
     return distances[-1][-1]
