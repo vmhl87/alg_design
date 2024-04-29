21ab8f7495cf54a0ed233d3049ff30b87ca7cb65
Vincent Loh <vincentmloh87@gmail.com>
2024-04-29 14:59:14 -0400

    notes

--- string-dist/string-dist.py
+++ string-dist/string-dist.py
@@ -1,22 +1,31 @@
 # string distance, bottom up dynamic
 
-def string_distance(s1, s2):
+def string_distance(s1, s2, verbose=False):
+    # create 2d state array - zeroed out
     distances = [[0 for j in range(len(s1)+1)] for i in range(len(s2)+1)]
 
+    # fill first row and first column - these correspond to comparing
+    # one prefix to the other empty prefix, and will be equal to the
+    # length of the nonzero prefix
     for j in range(len(s1)+1):
         distances[0][j] = j
-
     for i in range(1, len(s2)+1):
         distances[i][0] = i
 
+    # iterate over all other cells and set value
     for i in range(len(s2)):
         for j in range(len(s1)):
-            distances[i+1][j+1] = min(
-                    min(
-                        distances[i+1][j]+1,
-                        distances[i][j+1]+1
-                    ),
+            # min of up+1, left+1, diagonal+cost
+            distances[i+1][j+1] = min([
+                    distances[i+1][j]+1,
+                    distances[i][j+1]+1,
                     distances[i][j]+(0 if s2[i]==s1[j] else 1)
-                )
+                ])
 
+    # verbose printout
+    if verbose:
+        for row in distances:
+            print(' '.join([str(i) for i in row]))
+
+    # return bottom right cell corresponding to full word vs full word
     return distances[-1][-1]
