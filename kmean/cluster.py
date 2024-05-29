885f0f062ccf97afcb6664f2c76c6d10b7d515e8
Vincent Loh <vincentmloh87@gmail.com>
2024-05-29 13:41:48 -0400

    documentation

--- kmean/cluster.py
+++ kmean/cluster.py
@@ -1,3 +1,4 @@
+# hardcoded point values
 points = [
         [4, 21],
         [5, 19],
@@ -16,6 +17,8 @@ points = [
         [10, 10]
     ]
 
+
+# compute upper and lower bounds on x, y
 dim = [points[0][0], points[0][1], points[0][0], points[0][1]]
 
 for point in points:
@@ -23,24 +26,35 @@ for point in points:
         dim[i] = min(dim[i], point[i])
         dim[i+2] = max(dim[i+2], point[i])
 
+
+# store color in points array
 for point in points: point.append(0)
 
+
+# centroid datastructure, manages everything internally
 class Centroid:
     def __init__(self, x, y):
+        # current position
         self.p = [x, y]
+        # aggregated position from sub-points and count
+        # (pre-average)
         self._p = [0, 0, 0]
+        # child counts
         self.c, self._c = 0, 0
 
+    # reset for new iteration - clear preaverage and rotate counts
     def reset(self):
         self._p = [0, 0, 0]
         self._c, self.c = self.c, 0
 
+    # concatenate a point to self - aggregate position
     def cat(self, point):
         for i in range(2):
             self._p[i] += point[i]
 
         self._p[2] += 1
 
+    # average out and return if changes occured
     def reap(self):
         if self._p[2] == 0:
             return False
@@ -54,6 +68,8 @@ class Centroid:
 
         return True
 
+
+# randomly seed initial centroids
 import random
 centroids = []
 for i in range(3):
@@ -62,39 +78,52 @@ for i in range(3):
             random.uniform(dim[1], dim[3])
         ))
 
+
+# helper for distance squared
 def distance(a, b):
     return sum([(a[i]-b.p[i])**2 for i in range(2)])
 
+
+# main loop - repeat until break
 while True:
+    # reset all centroids
     for centroid in centroids:
         centroid.reset()
 
+    # for each point, compute closest centroid
     for point in points:
         best, choice = distance(point, centroids[0]), 0
 
-        for i in range(len(centroids)):
+        for i in range(1, len(centroids)):
             new = distance(point, centroids[i])
 
             if new < best:
                 best, choice = new, i
 
+        # update both color of point and centroid aggr
         centroids[choice].cat(point)
         point[2] = choice
 
+    # check if differences
     diff = False
 
     for centroid in centroids:
         if centroid.reap():
             diff = True
 
+    # if nothing changed, end loop
     if not diff: break
 
+
+# write out results
 f = open(".out.txt", "w")
 
 f.write(str(len(points)) + '\n')
 
+# renormalize bounds to [x, y, w, h]
 for i in range(2): dim[i+2] -= dim[i]
 
+# write out point coordinates normalized to 400x400 square
 for point in points:
     relative = [(point[i]-dim[i])/dim[i+2] for i in range(2)]
 
@@ -103,10 +132,12 @@ for point in points:
 
 f.write(str(len(centroids)) + '\n')
 
+# repeat for centroids
 for j in range(len(centroids)):
     relative = [(centroids[j].p[i]-dim[i])/dim[i+2] for i in range(2)]
 
     for i in range(2): f.write(str(int(relative[i]*400)) + ' ')
     f.write(str(j) + '\n')
 
+# close file stream
 f.close()
--- kmean/view.c
+++ kmean/view.c
@@ -1,30 +1,35 @@
 #include <stdlib.h>
+
+// framebuffer libs  -  https://github.com/vmhl87/fbgui
 #include "tgui.c"
 #include "tgin.c"
 
+
 int main(){
+	// open coloring file and read
 	FILE *data = fopen(".out.txt", "r");
 
+	// points
 	int n; fscanf(data, "%d", &n);
-
 	int *points = malloc(sizeof(int) * n * 3);
-
 	for(int i=0; i<n; ++i)
 		fscanf(data, "%d %d %d", points+i*3, points+i*3+1, points+i*3+2);
 
+	// centroids
 	int m; fscanf(data, "%d", &m);
-
 	int *centroids = malloc(sizeof(int) * m * 3);
-
 	for(int i=0; i<m; ++i)
 		fscanf(data, "%d %d %d", centroids+i*3,
 			centroids+i*3+1, centroids+i*3+2);
 
+	// cleanup file stream
 	fclose(data);
 
+	// cleanup terminal
 	system("clear");
 	openfb();
 
+	// very simplistic pallete
 	int colors[7][3] = {
 		{255, 0, 0},
 		{0, 255, 0},
@@ -35,8 +40,10 @@ int main(){
 		{255, 255, 255}
 	};
 
+	// initial background
 	rect(24, 24, width-48, height-48, 10, 10, 10);
 
+	// draw points
 	for(int i=0; i<n; ++i){
 		rect(
 			width/2 - 203 + points[i*3],
@@ -48,6 +55,7 @@ int main(){
 		);
 	}
 
+	// draw centroids
 	for(int i=0; i<m; ++i){
 		line(
 			width/2 - 205 + centroids[i*3],
@@ -69,8 +77,10 @@ int main(){
 		);
 	}
 
+	// break and wait on input
 	char c; scanf("%c", &c);
 
+	// clear framebuffer and cleanup
 	blank();
 	closefb();
 }
